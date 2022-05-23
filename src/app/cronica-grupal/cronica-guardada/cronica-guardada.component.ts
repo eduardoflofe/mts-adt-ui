da701e5098a8f237b53df9cc33576cc70aa0243d
lfgregorio <105452956+lfgregorio@users.noreply.github.com>
2022-05-22 23:06:11 -0500

    se agrega coreecion en lineas

--- src/app/cronica-grupal/cronica-guardada/cronica-guardada.component.ts
+++ src/app/cronica-grupal/cronica-guardada/cronica-guardada.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, timer } from "rxjs";
+import { map, share } from "rxjs/operators";
+import { DatePipe } from "@angular/common";
 
 @Component({
   selector: 'app-cronica-guardada',
   templateUrl: './cronica-guardada.component.html',
   styleUrls: ['./cronica-guardada.component.css']
 })
-export class CronicaGuardadaComponent implements OnInit {
+
+export class CronicaGuardadaComponent implements OnInit, OnDestroy {
+
+  time = new Date();
+  rxTime = new Date();
+  intervalId: any;
+  subscription: Subscription | undefined;
+  months = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+  today:any;
+  day:any;
+  month:any;
+  year:any;
 
   constructor() { }
 
   ngOnInit(): void {
+    const currentDate = new Date();
+    this.day = currentDate.getDate();
+    this.month = currentDate.getMonth();
+    this.year = currentDate.getFullYear();
+    this.today = currentDate;
+
+    this.subscription = timer(0, 1000)
+      .pipe(
+        map(() => new Date()),
+        share()
+      )
+      .subscribe(time => {
+        this.rxTime = time;
+      });
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
 }
