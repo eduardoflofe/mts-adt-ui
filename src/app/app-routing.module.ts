79f1474b98c3d451d6c26dc284c21a8242e08ab8
Antonio Alcantar <antonio.alcantar@people-media.com.mx>
2022-05-24 11:34:13 -0500

    Se coloca seguridad nuevamente en el archivo de configuración de rutas de navegación. Se corrigen estilos.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,12 +18,12 @@ const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'recuperarpassword', component: RegistroComponent },
-  { path: 'busqueda', component: BusquedaNssComponent },
-  { path: 'tarjeta', component: AppTarjetaPresentacionComponent },
-  { path: 'busquedaEspecifica', component: CCGrupalEspecificaComponent },
-  { path: 'nuevaCronica', component: NuevaCronicaComponent },
-  { path: 'cronicaGuardada', component: CronicaGuardadaComponent },
-  { path: 'consulta-cronica-grupal', component: ConsultaComponent },
+  { path: 'busqueda', component: BusquedaNssComponent, canActivate: [SeguridadRouter] },
+  { path: 'tarjeta', component: AppTarjetaPresentacionComponent, canActivate: [SeguridadRouter] },
+  { path: 'busquedaEspecifica', component: CCGrupalEspecificaComponent, canActivate: [SeguridadRouter] },
+  { path: 'nuevaCronica', component: NuevaCronicaComponent, canActivate: [SeguridadRouter] },
+  { path: 'cronicaGuardada', component: CronicaGuardadaComponent, canActivate: [SeguridadRouter] },
+  { path: 'consulta-cronica-grupal', component: ConsultaComponent, canActivate: [SeguridadRouter] },
   { path: '**', redirectTo: 'login' }
 ];
 
