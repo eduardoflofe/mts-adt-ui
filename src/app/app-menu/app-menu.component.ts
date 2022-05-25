8a4607fd5540efbdd3f46888bdd84435ae3df3ce
Antonio Alcantar <antonio.alcantar@people-media.com.mx>
2022-05-25 09:37:53 -0500

    Se agrega funcionalidad de selección activa de menu.

--- src/app/app-menu/app-menu.component.ts
+++ src/app/app-menu/app-menu.component.ts
@@ -8,21 +8,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./app-menu.component.css']
 })
 export class AppMenuComponent implements OnInit {
-
+  selectedCronica: boolean = false;
   constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
   pacientesRuta() {
+    this.selectedCronica = false;
     this.router.navigate(['busqueda'], { skipLocationChange: true });
   }
 
-  irCronicaGrupal() {
+  irCronicaGrupal(select: boolean) {
+    this.selectedCronica = select;
     this.router.navigate(["consulta-cronica-grupal"], { skipLocationChange: true });
   }
 
   irBusquedaDePacientesPorNss() {
+    this.selectedCronica = false;
     this.router.navigate(["busqueda"], { skipLocationChange: true });
   }
 
