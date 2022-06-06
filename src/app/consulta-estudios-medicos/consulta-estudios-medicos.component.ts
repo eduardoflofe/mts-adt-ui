0238ac80b0c7887d1652c59565929ae7f9e9e905
lfgregorio <105452956+lfgregorio@users.noreply.github.com>
2022-06-06 10:59:55 -0500

    se agrega vista de consulta estudios medicos

--- src/app/consulta-estudios-medicos/consulta-estudios-medicos.component.ts
+++ src/app/consulta-estudios-medicos/consulta-estudios-medicos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/service/auth-service.service';
 
 @Component({
   selector: 'app-consulta-estudios-medicos',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultaEstudiosMedicosComponent implements OnInit {
 
-  constructor() { }
+  fechaSelected!: string;
+  page: number = 1;
+  pageSize: number = 15;
+  resultadoTotal: number = 0;
+  dtOptions: DataTables.Settings = {};
+  numitems: number = 15;
+  order: string = 'desc';
+
+
+
+  tabla = [
+    {"Fecha":"20/06/2022", "NombreDeEstudio":"lorem imput dolor sit amen lorem imput", "PersonalSolicitante":"Lorent insump sint name"},
+    {"Fecha":"20/06/2022", "NombreDeEstudio":"lorem imput dolor sit amen lorem imput", "PersonalSolicitante":"Lorent insump sint name"},
+    {"Fecha":"20/06/2022", "NombreDeEstudio":"lorem imput dolor sit amen lorem imput", "PersonalSolicitante":"Lorent insump sint name"},
+    {"Fecha":"20/06/2022", "NombreDeEstudio":"lorem imput dolor sit amen lorem imput", "PersonalSolicitante":"Lorent insump sint name"},
+    {"Fecha":"20/06/2022", "NombreDeEstudio":"lorem imput dolor sit amen lorem imput", "PersonalSolicitante":"Lorent insump sint name"},
+    {"Fecha":"20/06/2022", "NombreDeEstudio":"lorem imput dolor sit amen lorem imput", "PersonalSolicitante":"Lorent insump sint name"},
+    {"Fecha":"20/06/2022", "NombreDeEstudio":"lorem imput dolor sit amen lorem imput", "PersonalSolicitante":"Lorent insump sint name"},
+    {"Fecha":"20/06/2022", "NombreDeEstudio":"lorem imput dolor sit amen lorem imput", "PersonalSolicitante":"Lorent insump sint name"},
+    {"Fecha":"20/06/2022", "NombreDeEstudio":"lorem imput dolor sit amen lorem imput", "PersonalSolicitante":"Lorent insump sint name"},
+    {"Fecha":"20/06/2022", "NombreDeEstudio":"lorem imput dolor sit amen lorem imput", "PersonalSolicitante":"Lorent insump sint name"},
+    {"Fecha":"20/06/2022", "NombreDeEstudio":"lorem imput dolor sit amen lorem imput", "PersonalSolicitante":"Lorent insump sint name"},
+    {"Fecha":"20/06/2022", "NombreDeEstudio":"lorem imput dolor sit amen lorem imput", "PersonalSolicitante":"Lorent insump sint name"},
+    {"Fecha":"20/06/2022", "NombreDeEstudio":"lorem imput dolor sit amen lorem imput", "PersonalSolicitante":"Lorent insump sint name"},
+    {"Fecha":"20/06/2022", "NombreDeEstudio":"lorem imput dolor sit amen lorem imput", "PersonalSolicitante":"Lorent insump sint name"},
+];
+constructor(private router: Router,
+  private authService: AuthService) {}
 
   ngOnInit(): void {
   }
 
+  irDetalle(cronicaGrupal: any) {
+
+  }
+
+
+  irNuevoEstudio(){
+
+  }
 }
