37edc9c8d7cff8f5328afcedb2ee462062b52ed8
lfgregorio <105452956+lfgregorio@users.noreply.github.com>
2022-06-07 13:43:07 -0500

    se agrega separación de formularios en estudio medico guardado

--- src/app/estudio-medico-guardado/estudio-medico-guardado.component.ts
+++ src/app/estudio-medico-guardado/estudio-medico-guardado.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./estudio-medico-guardado.component.css']
 })
 export class EstudioMedicoGuardadoComponent implements OnInit {
-
+  datosGenerales = false;
+  datosFamiliar = false;
+  datosExploracionCaso = false;
   constructor() { }
 
   ngOnInit(): void {
+    this.datosGenerales = true
+    this.datosFamiliar = false
+    this.datosExploracionCaso = false
   }
 
+  irDatosDeFamiliar(){
+    this.datosGenerales = false
+    this.datosFamiliar = true
+    this.datosExploracionCaso = false
+  }
+
+  irExploracionCaso(){
+    this.datosGenerales = false
+    this.datosFamiliar = false
+    this.datosExploracionCaso = true
+  }
+
+  irDatosGenerales(){
+    this.datosGenerales = true
+    this.datosFamiliar = false
+    this.datosExploracionCaso = false
+  }
 }
